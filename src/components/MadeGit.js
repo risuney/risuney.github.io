cb25b16cdb6cf9e6db16bb8df6d2d90d4ac183f4
risuney <honey197692@gmail.com>
2021-09-05 13:04:32 +0900

    fix: repoName

--- src/components/MadeGit.js
+++ src/components/MadeGit.js
@@ -5,7 +5,7 @@ class MadeGit extends Component {
     return (
       <a href={`https://github.com/risuney/${this.props.repoName}`} target="_blank" rel="noopener noreferrer">
         <img
-          src={`https://github-readme-stats.vercel.app/api/pin/?username=risuney&repo=${this.props.girepoName}&theme=dark&hide_border=true`} className="github-stats"
+          src={`https://github-readme-stats.vercel.app/api/pin/?username=risuney&repo=${this.props.repoName}&theme=dark&hide_border=true`} className="github-stats"
         />
       </a>
     )
